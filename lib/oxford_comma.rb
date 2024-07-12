f5e12c042ce82d2530a564c3c4527d96958e1c5a
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-07-12 12:01:57 -0400

    Complete lab

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length <= 2
+    array.join(' and ')
+  elsif array.length >= 3
+    last_item = array.pop
+    array.push("and #{last_item}")
+    array.join(", ")
+  else
+    array.join
+  end
 end
